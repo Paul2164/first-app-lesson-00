3330220a13f395af3586e6a42e62ae29be8b351a
Paul <fernandodeangel@yahoo.com>
2024-01-23 20:10:08 +0100

    CommonModule was add to HomeComponent imports

--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import {Input} from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housinglocation',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housinglocation works!
-    </p>
+    
+  <section class="listing">
+    <img class="listing-photo" [src]="housinglocation.photo"
+    alt="Exterior photo of {{housinglocation.name}}">
+    <h2 class="listing-heading">{{housinglocation.name}}</h2>
+    <p class="listing-location">{{housinglocation.city}},
+    {{housinglocation.state}}</p>
+  </section>
   `,
   styleUrl: './housinglocation.component.css'
 })
 export class HousinglocationComponent {
-
+  @Input() housinglocation! : HousingLocation;
 }
